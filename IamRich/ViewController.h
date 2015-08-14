f15a73a5a6c13274de2191411758f6736c0d9465
Nguyen Duy Khanh <nguyenduykhanh@Nguyens-MacBook-Pro.local>
2015-08-14 08:44:43 +0700

    Initial Commit

---
+++ IamRich/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IamRich
+//
+//  Created by Nguyen Duy Khanh on 8/14/15.
+//  Copyright (c) 2015 Nguyen Duy Khanh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IamRich/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  IamRich
+//
+//  Created by Nguyen Duy Khanh on 8/14/15.
+//  Copyright (c) 2015 Nguyen Duy Khanh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
